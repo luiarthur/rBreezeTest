c7fd562615eb4bb8bfb8159cd730e73f9fa0aeff
Arthur Lui <luiarthur@gmail.com>
2017-04-15 12:27:54 -0700

    yes

--- R/zzz.R
+++ R/zzz.R
@@ -6,10 +6,10 @@
 
   if (no_jar) {
     # Compile the jar from src
-
-    # Edit these lines accordingly
-    cd_path <- paste0("cd ",lib_java_path, "/rBreeze;")
     compile_jar <- "sbt assembly;"
+                                           #TO EDIT#
+    cd_path <- paste0("cd ",lib_java_path, "/rBreeze;")
+                         ########## TO EDIT ##################
     mv_jar <- "mv target/scala-2.11/rBreeze-assembly-0.1.0.jar ../"
 
     system(paste(cd_path, compile_jar, mv_jar))
