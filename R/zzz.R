ea592e48b83b6a07f7a6b974276ba2e4ba3d8945
Arthur Lui <luiarthur@gmail.com>
2017-04-15 11:55:22 -0700

    yes

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .onLoad <- function(libname, pkgname) {
   lib_java_path <- paste0(libname, "/", pkgname, "/java")
-  no_jar <- identical(list.files(lib_java_path), character(0))
+  jars <- grep("*.jar", list.files())
+  no_jar <- identical(jars, integer(0))
   if (no_jar) {
     # Compile the jar from src
     system(paste0(".",lib_java_path,"/genJar"))
