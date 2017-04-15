9f0383ea7dc96d6f0fa75ee76234a0438813afc3
Arthur Lui <luiarthur@gmail.com>
2017-04-15 12:27:41 -0700

    yes

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,20 @@
 .onLoad <- function(libname, pkgname) {
+
   lib_java_path <- paste0(libname, "/", pkgname, "/java")
   jars <- grep("*.jar", list.files(lib_java_path))
   no_jar <- identical(jars, integer(0))
+
   if (no_jar) {
     # Compile the jar from src
+
+    # Edit these lines accordingly
     cd_path <- paste0("cd ",lib_java_path, "/rBreeze;")
     compile_jar <- "sbt assembly;"
     mv_jar <- "mv target/scala-2.11/rBreeze-assembly-0.1.0.jar ../"
+
     system(paste(cd_path, compile_jar, mv_jar))
   }
+
   rscala::.rscalaPackage(pkgname)
+
 }
